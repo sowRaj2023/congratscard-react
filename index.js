f755b6d4a4340e33ee020d34a0a15fe8bafbe540
sowRaj2023 <sownyuvan1180@gmail.com>
2024-07-11 23:39:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+    <div className="bg-container">
+        <h1>Congratulations</h1>
+        <div className="sub-container">
+            <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" className="profile-image" />
+            <h1>KIRAN V</h1>
+            <p>Vishnu Institue of COmputer Education and Technology, Bhimavaram</p>
+            <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" className="profile-image" />
+        </div>
+
+    </div>
+)
+ReactDOM.render(element, document.getElementById("root"));
